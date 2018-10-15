8b5f390a1007106e3d319131ea27554278171245
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-10-15 18:00:40 +0900

    make controller

---
+++ app/controllers/crawling_controller.rb
@@ -0,0 +1,244 @@
+# encoding: utf-8
+require 'open-uri'
+
+class CrawlingController < ApplicationController
+
+  def foursquare
+    log = Logger.new '/home/tomohiroo/pecopeco/shared/log/crawling.log'
+    msg = 'クローリングを開始します。'
+    slack_notify msg
+    log.info msg
+    $account_num = 1
+    $client_id = Rails.application.secrets.crawling_ids[$account_num-1]
+    $client_secret = Rails.application.secrets.crawling_secrets[$account_num-1]
+    main log
+  end
+
+  DOMAIN = "https://api.foursquare.com/v2"
+
+  private
+
+    def search params
+      conn = Faraday.new url: "#{DOMAIN}/venues/search"
+      conn.get do |req|
+        req.params[:client_id] = $client_id
+        req.params[:client_secret] = $client_secret
+        req.params[:v] = Rails.application.secrets.foursquare_version
+        req.params[:locale] = "ja"
+        req.params[:intent] = "browse"
+        req.params[:ll] = params[:ll]
+        req.params[:limit] = params[:limit]
+        req.params[:radius] = params[:radius]
+        req.params[:categoryId] = params[:categoryId]
+      end
+    end
+
+    def get_details ids
+      restaurant_hashes = []
+      hydra = Typhoeus::Hydra.new
+      ids.each do |id|
+        request = Typhoeus::Request.new(
+          "#{DOMAIN}/venues/#{id}",
+          followlocation: true,
+          params: {
+            client_id: $client_id,
+            client_secret: $client_secret,
+            v: Rails.application.secrets.foursquare_version,
+            locale: "ja"
+          }
+        )
+        request.on_complete do |response|
+          status = JSON.parse(response.body)["meta"]["code"]
+          return status if status == 429
+          venue = JSON.parse(response.body)["response"]["venue"]
+          if venue["id"].blank?
+            slack_notify "errorが起きました。"
+            slack_notify venue
+            log.info venue
+          end
+          new_restaurant, category, pictures, station = Restaurant.build_with_foursquare_hash venue
+          detail = new_restaurant.attributes
+          detail[:category] = category
+          detail[:pictures] = pictures
+          detail[:station] = station
+          restaurant_hashes << { restaurant: new_restaurant, detail: detail }
+        end
+        hydra.queue(request)
+      end
+      hydra.run
+      restaurant_hashes
+    end
+
+    def get_restaurants params
+      response = search params
+      return response.status, response.body, 0, 'search api' if response.status != 200
+      foursquare_ids = JSON.parse(response.body)["response"]["venues"].map { |h| h["id"] }
+      return 200, nil, 0, '' if foursquare_ids.blank?
+      db_foursquare_ids = Restaurant.where(foursquare_id: foursquare_ids).select(:foursquare_id).map(&:foursquare_id)
+      new_restaurants_foursquare_ids = foursquare_ids.select { |id| db_foursquare_ids.exclude? id }
+      new_restaurants_hash = get_details new_restaurants_foursquare_ids
+      return 429, new_restaurants_hash, 0, 'detail api' if new_restaurants_hash == 429
+      Restaurant.import new_restaurants_hash.map { |h| h[:restaurant] }, recursive: true, validate: false
+      return 200, nil, new_restaurants_foursquare_ids.length, ''
+    end
+
+    def finishing_processing log, lat, lng, count
+      info = <<-EOC
+
+    ==============================================================
+    処理を終了します。
+    lat: #{lat}
+    lng: #{lng}
+    count: #{count} / 48279 (#{(count / 48279.0 * 10000).round / 100.0}%)
+    Google Maps: "https://www.google.co.jp/maps/search/#{lat},#{lng}?sa=X&ved=2ahUKEwjvx7jJq4LeAhUIIIgKHSD-CTsQ8gEwAHoECAAQAQ"
+
+    DBのレストランの件数: #{Restaurant.count}
+    ==============================================================
+      EOC
+      log.info info
+      slack_notify info
+    end
+
+    def complete_processing log, lat, lng, count
+      info = <<-EOC
+
+    ==============================================================
+    領域内のクローリングが全て完了しました！！！。
+    lat: #{lat}
+    lng: #{lng}
+    Google Maps: "https://www.google.co.jp/maps/search/#{lat},#{lng}?sa=X&ved=2ahUKEwjvx7jJq4LeAhUIIIgKHSD-CTsQ8gEwAHoECAAQAQ"
+    count: #{count} / 48279 (#{(count / 48279.0 * 10000).round / 100.0}%)
+
+    DBのレストランの件数: #{Restaurant.count}
+    ==============================================================
+      EOC
+      log.info info
+      slack_notify info
+    end
+
+    def slack_notify info
+      slack_webhook_url = Rails.application.secrets.slack_webhook_url
+      notifier = Slack::Notifier.new slack_webhook_url do
+        defaults username: "クローリングマン"
+      end
+      notifier.post({
+        text: "```【#{Rails.env}】\n#{info}```",
+        icon_url: 'https://t14.pimg.jp/030/224/824/1/30224824.jpg'
+      })
+    end
+
+    def main log
+      json_file_path = Rails.root.join "public/crawling.json"
+      json_data = File.open(json_file_path) { |j| JSON.load j }
+      lat = json_data["lat"]
+      lng = json_data["lng"]
+
+      restaurant_category_id = '4d4b7105d754a06374d81259,4bf58dd8d48988d116941735'
+      params = {
+        ll: "#{lat},#{lng}",
+        limit: 50,
+        radius: 50,
+        categoryId: "#{restaurant_category_id}"
+      }
+
+      response_code, response_body, get_restaurants_num, error_api = get_restaurants params
+
+      if response_code == 429
+        info = <<-EOC
+
+    =========================================================
+    foursquare (apikey: #{$client_secret}) の回数上限に達しました。
+    lat: #{lat}
+    lng: #{lng}
+    response_code: #{response_code}
+    response_body: #{response_body}
+    errorが起きたapi: #{error_api}
+    保存したレストランの件数: #{get_restaurants_num}
+    DBのレストランの件数: #{Restaurant.count}
+    lat: #{lat}
+    lng: #{lng}
+    count: #{json_data["count"]} / 48279 (#{(json_data["count"] / 48279.0 * 10000).round / 100.0}%)
+    =========================================================
+        EOC
+
+        log.info info
+        slack_notify info
+        return finishing_processing log, lat, lng, json_data["count"] unless $account_num < Rails.application.secrets.crawling_ids.length
+        $account_num += 1
+        $client_id = Rails.application.secrets.crawling_ids[$account_num-1]
+        $client_secret = Rails.application.secrets.crawling_secrets[$account_num-1]
+      elsif response_code != 200
+        info = <<-EOC
+
+    =========================================================
+    foursquareからエラーが返ってきました。
+    処理は続行しています。
+    lat: #{lat}
+    lng: #{lng}
+    count: #{json_data["count"]} / 48279 (#{(json_data["count"] / 48279.0 * 10000).round / 100.0}%)
+    DBのレストランの件数: #{Restaurant.count}
+    一番最後に保存したレストラン: #{Restaurant.last.attributes}
+    response_code: #{response_code}
+    response_body: #{response_body}
+    errorが起きたapi: #{error_api}
+    =========================================================
+        EOC
+
+        log.info info
+        slack_notify info
+      else
+        log.info <<-EOC
+
+    保存したレストランの件数: #{get_restaurants_num}
+    lat: #{lat}
+    lng: #{lng}
+    count: #{json_data["count"]} (#{(json_data["count"] / 48279.0 * 10000).round / 100.0}%)
+        EOC
+      end
+      south_end = 35.582135
+      east_end = 139.811738
+      lat_step = 0.00090128  # 100m
+      lng_step = 0.00055278  # 50m
+      initial_lat = 35.745434
+
+      if lat < south_end
+        if lng > east_end
+          return complete_processing(log, lat, lng, json_data["count"])
+        end
+
+        info = <<-EOC
+
+    =========================================================
+    改行します
+    lat: #{lat}
+    lng: #{lng}
+    count: #{json_data["count"]} / 48279 (#{(json_data["count"] / 48279.0 * 10000).round / 100.0}%)
+
+    DBのレストランの件数: #{Restaurant.count}
+    =========================================================
+        EOC
+
+        log.info info
+        slack_notify info
+
+        json_data["lng"] += lng_step
+        json_data["line_num"] += 1
+        if json_data["line_num"].odd?
+          json_data["lat"] = initial_lat
+        else
+          json_data["lat"] = initial_lat - lat_step / 2
+        end
+      else
+        json_data["lat"] -= lat_step
+      end
+
+      json_data["count"] += 1
+      open(json_file_path, 'w') do |io|
+        JSON.dump(json_data, io)
+      end
+
+      sleep rand 10
+      main log
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,3 @@
-# == Route Map
-#
-#                    Prefix Verb   URI Pattern                                                                              Controller#Action
-#         komachi_heartbeat        /ops                                                                                     KomachiHeartbeat::Engine
-#                  v1_users GET    /v1/users(.:format)                                                                      v1/users#index {:format=>:json}
-#                           POST   /v1/users(.:format)                                                                      v1/users#create {:format=>:json}
-#    distance_v1_restaurant GET    /v1/restaurants/:id/distance(.:format)                                                   v1/restaurants#distance {:format=>:json}
-#            v1_restaurants GET    /v1/restaurants(.:format)                                                                v1/restaurants#index {:format=>:json}
-#            share_v1_clips POST   /v1/clips/share(.:format)                                                                v1/clips#share {:format=>:json}
-#                  v1_clips GET    /v1/clips(.:format)                                                                      v1/clips#index {:format=>:json}
-#                           POST   /v1/clips(.:format)                                                                      v1/clips#create {:format=>:json}
-#                   v1_clip GET    /v1/clips/:id(.:format)                                                                  v1/clips#show {:format=>:json}
-#                           PATCH  /v1/clips/:id(.:format)                                                                  v1/clips#update {:format=>:json}
-#                           PUT    /v1/clips/:id(.:format)                                                                  v1/clips#update {:format=>:json}
-#                           DELETE /v1/clips/:id(.:format)                                                                  v1/clips#destroy {:format=>:json}
-#                 v1_boards GET    /v1/boards(.:format)                                                                     v1/boards#index {:format=>:json}
-#                           POST   /v1/boards(.:format)                                                                     v1/boards#create {:format=>:json}
-#                  v1_board GET    /v1/boards/:id(.:format)                                                                 v1/boards#show {:format=>:json}
-#                           PATCH  /v1/boards/:id(.:format)                                                                 v1/boards#update {:format=>:json}
-#                           PUT    /v1/boards/:id(.:format)                                                                 v1/boards#update {:format=>:json}
-#                           DELETE /v1/boards/:id(.:format)                                                                 v1/boards#destroy {:format=>:json}
-#        v1_clip_categories POST   /v1/clip_categories(.:format)                                                            v1/clip_categories#create {:format=>:json}
-#          v1_clip_category DELETE /v1/clip_categories/:id(.:format)                                                        v1/clip_categories#destroy {:format=>:json}
-#             v1_categories GET    /v1/categories(.:format)                                                                 v1/categories#index {:format=>:json}
-#               v1_stations GET    /v1/stations(.:format)                                                                   v1/stations#index {:format=>:json}
-#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
-#
-# Routes for KomachiHeartbeat::Engine:
-#    heartbeat GET  /heartbeat(.:format)    komachi_heartbeat/heartbeat#index {:format=>"txt"}
-#      version GET  /version(.:format)      komachi_heartbeat/heartbeat#version
-# stats_worker GET  /stats/worker(.:format) komachi_heartbeat/stats#worker
-
 Rails.application.routes.draw do
+  root to: 'crawling#foursquare'
 end
