cd016224da019095f0f1bdddc60d8aef409822ef
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-11-18 14:41:34 +0900

    fix slack notify

--- app/controllers/crawling_controller.rb
+++ app/controllers/crawling_controller.rb
@@ -157,11 +157,11 @@ DBのレストランの件数: #{Restaurant.count}
     def slack_notify info
       slack_webhook_url = ENV['slack_webhook_url']
       notifier = Slack::Notifier.new slack_webhook_url do
-        defaults username: "クローリングマン"
+        defaults username: "Foursquareクローラー"
       end
       notifier.post(
-        text: "```【#{Rails.env}】\n#{info}```",
-        icon_url: 'https://t14.pimg.jp/030/224/824/1/30224824.jpg'
+        text: "```#{info}```",
+        icon_url: 'https://cdn4.iconfinder.com/data/icons/socialcones/508/FourSquare-512.png'
       )
     end
 
