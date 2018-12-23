cf50749843f81a3f057a3df3f8c6be3d5a4cdbd3
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-12-23 23:00:53 +0900

    change Scraping#format_tabelog_url to class method

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,32 +1,34 @@
 require 'open-uri'
 
 class Scraping
-  def self.my_ip
-    JSON.parse(Faraday.new(url: 'https://jsonip.com').get.body)['ip']
-  end
-
-  def self.set_global_variable(params)
-    $lat = params[:lat].to_f
-    $lng = params[:lng].to_f
-    $line_num = params[:line_num].to_i
-    $count = params[:count].to_i
-    $account_num = 1
-    $crawling_ids = []
-    $crawling_secrets = []
-    ENV['crawling_ids'].split(',').shuffle.each do |ids|
-      $crawling_ids.push ids.split(':')[0]
-      $crawling_secrets.push ids.split(':')[1]
+  class << self
+    def my_ip
+      JSON.parse(Faraday.new(url: 'https://jsonip.com').get.body)['ip']
     end
-    $client_id = $crawling_ids[$account_num-1]
-    $client_secret = $crawling_secrets[$account_num-1]
-    $error_count = 0
-    $restaurant_number = Restaurant.count
-  end
 
-  def format_tabelog_url(url)
-    uri = URI.parse(url)
-    uri.host = 'tabelog.com' if uri.host == 's.tabelog.com'
-    uri.query = nil
-    uri.to_s
+    def set_global_variable(params)
+      $lat = params[:lat].to_f
+      $lng = params[:lng].to_f
+      $line_num = params[:line_num].to_i
+      $count = params[:count].to_i
+      $account_num = 1
+      $crawling_ids = []
+      $crawling_secrets = []
+      ENV['crawling_ids'].split(',').shuffle.each do |ids|
+        $crawling_ids.push ids.split(':')[0]
+        $crawling_secrets.push ids.split(':')[1]
+      end
+      $client_id = $crawling_ids[$account_num-1]
+      $client_secret = $crawling_secrets[$account_num-1]
+      $error_count = 0
+      $restaurant_number = Restaurant.count
+    end
+
+    def format_tabelog_url(url)
+      uri = URI.parse(url)
+      uri.host = 'tabelog.com' if uri.host == 's.tabelog.com'
+      uri.query = nil
+      uri.to_s
+    end
   end
 end
